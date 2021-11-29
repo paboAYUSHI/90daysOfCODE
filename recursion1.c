d93b0f28a302b139f41f713e403ac41bfebc3169
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2021-11-29 23:56:40 +0530

    Add files via upload

---
+++ recursion1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fun(int);
+int main()
+{
+    int n,z;
+    printf("number: ");
+    scanf("%d",&n);
+    z=fun(n);
+     printf("%d",z);
+    return 0;
+}
+int fun(int n){
+    //int count=0;
+    if(n==1)
+    return 1;
+    else
+    return n*fun(n-1);
+}
