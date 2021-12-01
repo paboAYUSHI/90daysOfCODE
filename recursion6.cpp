ee6ade8e3076e0e2a37dd41f4519ca1af699f902
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2021-12-01 20:04:02 +0530

    Add files via upload

---
+++ recursion6.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int prime(int number , int i){
+    if(number<=2){
+        return (number==2) ?true:false;
+    }
+    else if(number%2==0)
+       return false;
+    else if(i*i>number)
+       return true;
+
+    return prime(number,i+1);
+}
+int main(){
+    if(prime(15,2))
+      cout<<"true";
+    else
+      cout<<"false";
+}
