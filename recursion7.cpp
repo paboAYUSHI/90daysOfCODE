e1883851eadda39ded2fd86cd3d21b035a1d3c44
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2021-12-02 21:21:47 +0530

    Add files via upload

---
+++ recursion7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int function (int number){
+    //int sum=0;
+    if(number==0)
+      return 0;
+    else 
+      return (number%10)+function(number/10);
+}
+int main(){
+    int number;
+    cout<<"enter a number";
+    cin>>number;
+   int z= function(number);
+   cout<<z;
+}
