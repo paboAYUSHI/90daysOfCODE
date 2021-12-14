c3ef36cc30164afce1537b67649b609ae3bfabfd
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2021-12-14 12:12:21 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,16 @@
+//reversing an array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+    int A[N];
+    for(int i=0;i<N;i++){
+        cin>>A[i];
+    }
+    for(int i=N-1;i>=0;i--){
+        cout<<A[i];
+    }
+} 
+
