4a03917308ea777fd12c8016d2be0bb89264a9ea
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2021-12-19 08:50:39 +0530

    Add files via upload

---
+++ ds4.cpp
@@ -0,0 +1,17 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+	for(int i=0;i<T;i++){
+	    int a , b;
+	    cin>>a>>b;
+	    std::cout << a+b << std::endl;
+	}
+
+	return 0;
+}
