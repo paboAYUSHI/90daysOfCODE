851367c1a76235612f0d2caff0d5b5ec7bc2e06a
paboAYUSHI <93575942+paboAYUSHI@users.noreply.github.com>
2022-01-25 18:15:28 +0530

    Add files via upload

---
+++ flow2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int a,b;
+	    cin>>a>>b;
+	    int c;
+	    c=a%b;
+	    std::cout << c << std::endl;
+	}
+
+	return 0;
+}
